aae5db70a7464eed801abebd067918c98de1a705
demonassit <jminorg@ipn.mx>
2023-07-26 10:00:00 -0600

    apuntadores

---
+++ 04Apuntadores/apuntadorhola.cpp
@@ -0,0 +1,19 @@
+//ejemplo de puntero
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string saludo = "Hola mundito";
+	string *p;
+	//p = &saludo;
+	cout<< &saludo <<endl;
+	cout<< p << endl;
+	cout<< saludo << endl;
+	
+	
+	
+	
+}
